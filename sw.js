e85e03e87d4ed690eb2327ea134ced45fe274f89
NiceShot Sports Photography <72646670+marceloribeiro1711@users.noreply.github.com>
2026-07-01 21:13:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Padel Coaching — Service Worker
 // Compatível com Android 8 (Chrome 67+)
 
-const CACHE_VERSION = 'v1.1.93';
+const CACHE_VERSION = 'v1.1.94';
 
 const PRECACHE_URLS = [
   './',
